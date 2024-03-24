ee9dda60c8683d3f1fceb521e3efda6917b0955c
Viresh Mittal <vireshmittal2@gmail.com>
2024-03-23 22:42:44 -0500

    Revert "fixed main"
    This reverts commit c479b72f5bc267a4b9a069cdeeb1e2c4472acf44.

--- main.py
+++ main.py
@@ -12,23 +12,30 @@ import googlemaps.distance_matrix
 import pymongo 
 import streamlit.components.v1 as components
 
+with open('keys.txt') as f :
+    google_key= f.readline()
+    mongo_user=f.readline()
+    mongo_pwd=f.readline()
 mongo_client = pymongo.MongoClient(
-    "mongodb+srv://:@emissions-hackathon-pro.l9weice.mongodb.net/?retryWrites=true&w=majority&appName=emissions-hackathon-proj"
+    f"mongodb+srv://{mongo_user}:{mongo_pwd}@emissions-hackathon-pro.l9weice.mongodb.net/?retryWrites=true&w=majority&appName=emissions-hackathon-proj"
 )
-google_key=
 gmaps = googlemaps.Client(key=google_key)
+
+# if 'location' not in st.session_state:
+#     ip = requests.get('https://api64.ipify.org?format=json').json()['ip']
+#     response = requests.get(f'https://ipapi.co/{ip}/json/').json()
+#     st.session_state.location = (response['latitude'], response['longitude'])
 
 car_specified = False
 def car_suggestions(field, search_term):
     regex = f".*{search_term}.*"
     query = {field: {"$regex": regex, "$options": "i"}} 
-
-    if field == 'make':
-        query.update({'year': car_year})
+    
+    if field == "make":
+        query['year'] = car_year
     
     if field == 'model':
-        query.update({'year': car_year})
-        query.update({'make': car_make})
+        query['make'] = car_make
     
     return set([doc[field] for doc in mongo_client['vehicles-emissions']['vehicles'].find(query)])
 with st.sidebar:
@@ -39,13 +46,14 @@ with st.sidebar:
         if car_make:
             car_model = st_searchbox(lambda search: car_suggestions('model', search), placeholder='Model', key='5')
             car_specified = True
+
 if car_specified:
     result = mongo_client['vehicles-emissions']['vehicles'].find_one({'year': car_year, 'model': car_model, 'make': car_make})
 
 route = False
 def search_places(search_term):
     return [location['description'] for location in googlemaps.places.places_autocomplete(
-        client=gmaps, input_text=search_term + " ", radius=50000)]
+        client=gmaps, input_text=search_term + " ", location=st.session_state.location, radius=50000)]
 
 origin = st_searchbox(search_places, placeholder="Choose starting point...", key="1")
 destination = st_searchbox(search_places, placeholder="Choose destination...", key="2")
@@ -67,17 +75,13 @@ if car_specified and route and result:
     trees_required = total_emissions_pounds / CO2_OFFSET_PER_TREE_PER_YEAR
     trees_required_rounded_up = int(trees_required) if trees_required.is_integer() else int(trees_required) + 1  # Round up to the nearest whole number
 
-    if carbon_g < 1000:
-        st.write(f"Total estimated CO2 emissions for driving {distance_mi}les in a {car_year} {car_make} {car_model}: {carbon_g:.2f} grams.")
-    else:
-        carbon_kg = carbon_g/1000
-        st.write(f"Total estimated CO2 emissions for driving {distance_mi}les in a {car_year} {car_make} {car_model}: {carbon_kg:.2f} kilograms.")
+    st.write(f"Total estimated CO2 emissions for driving {distance_mi}les in a {car_year} {car_make} {car_model}: {carbon_g:.2f} grams.")
     st.write(f"To offset these emissions, you would need to plant approximately {trees_required_rounded_up} tree(s).")
 
     origin_url = origin.replace(' ', '+')
     destination_url = destination.replace(' ', '+')
 
-    embed_url = f"https://www.google.com/maps/embed/v1/directions?key={google_key}&origin={origin_url}&destination={destination_url}&mode=driving"
+    embed_url = f"https://www.google.com/maps/embed/v1/directions?key={google_key}-aU&origin={origin_url}&destination={destination_url}&mode=driving"
 
     st.markdown(f'<iframe width="600" height="450" frameborder="0" style="border:0" src="{embed_url}" allowfullscreen></iframe>',
                 unsafe_allow_html=True)
